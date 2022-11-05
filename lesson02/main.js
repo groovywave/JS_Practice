d0e488cff6bdd9e50720b463e9b1068978161ae7
groovywave <vacation555@gmail.com>
2022-11-06 01:48:16 +0900

    create main.js

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,1 +1,13 @@
 "use strict";
+ul = document.getElementById("js-target");
+
+const img = document.createComment<"img">;
+img.src = "bookmark.png";
+img.alt = "ブックマーク";
+
+const a = document.createElement<"a">;
+a.href = "1.html"
+a.textContent = "これです";
+
+a.appendChild(img);
+ul.appendChild(a);
