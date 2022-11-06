b35f25ed8a3dc4383bf51191b05e1382ee13addf
groovywave <vacation555@gmail.com>
2022-11-06 22:04:22 +0900

    create draft

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,13 +1,17 @@
 "use strict";
-ul = document.getElementById("js-target");
 
-const img = document.createComment<"img">;
+const ul = document.getElementById("js-target");
+
+const li = document.createElement("li");
+
+const a = document.createElement("a");
+a.href = "1.html";
+a.textContent = "これです";
+
+const img = document.createElement("img");
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-const a = document.createElement<"a">;
-a.href = "1.html"
-a.textContent = "これです";
-
 a.appendChild(img);
-ul.appendChild(a);
+li.appendChild(a);
+ul.appendChild(li);
