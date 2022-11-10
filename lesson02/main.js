fcd5545e4c76d52f5e1b890a6572b5779ba05f57
groovywave <vacation555@gmail.com>
2022-11-11 06:49:30 +0900

    delete 'use stridt'and comments

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const ul = document.getElementById("js-target");
 
 const li = document.createElement("li");
@@ -12,15 +10,6 @@ const img = document.createElement("img");
 img.src = "bookmark.png";
 img.alt = "ブックマーク";
 
-// <!--a has children-->
-// <!--check console.log(a.childrens);-->
-// a.insertBefore(img,a.firstChild);
-// li.appendChild(a);
-// ul.appendChild(li);
-
-// <!--refactoring-->
 ul.appendChild(li).appendChild(a).insertBefore(img,a.firstChild);
 
-// <!--advanced-->
-// ul.appendChild(li).appendChild(a).insertAdjacentElement("beforebegin",img);
-// https://itosae.com/js_lesson01/#toc7
+
