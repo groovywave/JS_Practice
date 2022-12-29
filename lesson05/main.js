44a90bec70cc244d13b0d1597d7e9ab490b82024
groove <vacation555@gmail.com>
2022-12-29 13:21:13 +0900

    mkdir

---
+++ lesson05/main.js
@@ -0,0 +1,17 @@
+const ul = document.getElementById("js-ul"); 
+const fragment = document.createDocumentFragment(); 
+const elements = [ 
+  {to: "bookmark.html", img:"img/1.png", alt:"画像1", text: "ブックマーク"}, 
+  {to: "message.html", img:"img/2.png", alt:"画像2", text: "メッセージ"} 
+]; 
+elements.forEach(function(element){ 
+  const li = document.createElement("li"); 
+  const a = document.createElement("a"); 
+  const img = document.createElement("img"); 
+  a.href = element.to; 
+  a.textContent = element.text; 
+  img.src = element.img; 
+  img.alt = element.alt; 
+  fragment.appendChild(li).appendChild(a).insertAdjacentElement("afterbegin",img); 
+}); 
+ul.appendChild(fragment);
