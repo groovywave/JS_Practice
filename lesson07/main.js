d4c7863b6e1c915a0087cae9d5a3e67be865f7d6
groove <vacation555@gmail.com>
2023-01-06 01:40:38 +0900

    feat: add loadingCirlce

--- lesson07/main.js
+++ lesson07/main.js
@@ -1,13 +1,14 @@
-new Promise((resolve)=> {
-  const elements = [ 
-    {to: "bookmark.html", img:"img/1.png", alt:"画像1", text: "ブックマーク"}, 
-    {to: "message.html", img:"img/2.png", alt:"画像2", text: "メッセージ"} 
-  ]; 
-  setTimeout(() => {
-    resolve(elements);
-  }, 3000);
-}).then((resolvedElements)=> {
-  const ul = document.getElementById("js-ul"); 
+const ul = document.getElementById("js-ul"); 
+const loadingCircle = document.createElement("img"); 
+
+function renderCircle(){
+  loadingCircle.src = "img/loading-circle.gif";
+  loadingCircle.alt = "loading-circle.gif";
+  ul.appendChild(loadingCircle);
+}  
+
+function renderContact(resolvedElements){
+  ul.removeChild(loadingCircle);
   const fragment = document.createDocumentFragment(); 
   for (const element of resolvedElements) { 
     const li = document.createElement("li"); 
@@ -23,4 +24,17 @@ new Promise((resolve)=> {
       .insertAdjacentElement("afterbegin",img); 
   }; 
   ul.appendChild(fragment);
+}
+
+new Promise((resolve)=> {
+  renderCircle();
+  const elements = [ 
+    {to: "bookmark.html", img:"img/1.png", alt:"画像1", text: "ブックマーク"}, 
+    {to: "message.html", img:"img/2.png", alt:"画像2", text: "メッセージ"} 
+  ]; 
+  setTimeout(() => {
+    resolve(elements);
+  }, 3000);
+}).then((resolvedElements)=> {
+  renderContact(resolvedElements);
 });
