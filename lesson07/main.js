d44ad89b8a7ae58f07590c1e98f7f48baf6ba367
groove <vacation555@gmail.com>
2023-01-06 01:01:08 +0900

    mkdir lesson07

--- lesson07/main.js
+++ lesson07/main.js
@@ -1,14 +1,13 @@
-const ul = document.getElementById("js-ul"); 
-const loadingCircle = document.createElement("img"); 
-
-function renderCircle(){
-  loadingCircle.src = "img/loading-circle.gif";
-  loadingCircle.alt = "loading-circle.gif";
-  ul.appendChild(loadingCircle);
-}  
-
-function renderContact(resolvedElements){
-  ul.removeChild(loadingCircle);
+new Promise((resolve)=> {
+  const elements = [ 
+    {to: "bookmark.html", img:"img/1.png", alt:"画像1", text: "ブックマーク"}, 
+    {to: "message.html", img:"img/2.png", alt:"画像2", text: "メッセージ"} 
+  ]; 
+  setTimeout(() => {
+    resolve(elements);
+  }, 3000);
+}).then((resolvedElements)=> {
+  const ul = document.getElementById("js-ul"); 
   const fragment = document.createDocumentFragment(); 
   for (const element of resolvedElements) { 
     const li = document.createElement("li"); 
@@ -19,22 +18,9 @@ function renderContact(resolvedElements){
     img.src = element.img; 
     img.alt = element.alt; 
     fragment
-    .appendChild(li)
-    .appendChild(a)
-    .insertAdjacentElement("afterbegin",img); 
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin",img); 
   }; 
   ul.appendChild(fragment);
-}
-
-new Promise((resolve)=> {
-  renderCircle();
-  const elements = [ 
-    {to: "bookmark.html", img:"img/1.png", alt:"画像1", text: "ブックマーク"}, 
-    {to: "message.html", img:"img/2.png", alt:"画像2", text: "メッセージ"} 
-  ]; 
-  setTimeout(() => {
-    resolve(elements);
-  }, 3000);
-}).then((resolvedElements)=> {
-  renderContact(resolvedElements);
 });
