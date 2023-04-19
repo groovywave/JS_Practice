fce49b6982bf36e7d3fc00fae51ee0c6563df123
groove <vacation555@gmail.com>
2023-04-20 01:50:59 +0900

    remove files in repository

--- tests/tests/sample.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByRole('combobox', { name: '検索' }).click();
-  await page.getByRole('combobox', { name: '検索' }).fill('');
-  await page.getByRole('combobox', { name: '検索' }).press('Enter');
-  await page.screenshot({ path:`test.png` });
-});
