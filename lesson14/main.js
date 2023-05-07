e5c3f5a42680ed17aae0a3ac36c5523c114fffd1
groove <vacation555@gmail.com>
2023-05-07 22:32:28 +0900

    Add const as halfWidthDigits

--- lesson14/main.js
+++ lesson14/main.js
@@ -8,16 +8,7 @@ const modal = document.getElementById("js-modal");
 const mask = document.getElementById("js-mask");
 const promptMessage = document.getElementById("js-prompt-message");
 const inputBox = document.getElementById("js-input-box");
-const validationTerms = {
-  name: {
-    halfWidthDigits,
-  },
-  digits: {
-    pattern: /^(?<halfWidthDigits>-?[0-9]+(\.?[0-9]*))$/,
-  },
-};
-// console.log("test");
-// console.log(validationTerms.digits);
+const halfWidthDigits = /^-?[0-9]+(\.?[0-9]*)$/;
 const url = "https://mocki.io/v1/1c058349-634e-462a-ad37-14f135e59b99";
 // const url = ""; //Not JSON
 // const url = "https://mocki.io/v1/55dc6233-a8fe-44ca-8906-3de313545ce8"; //No data
@@ -145,11 +136,7 @@ backButton.addEventListener("click", () => {
 
 function checkInput() {
   const inputNumber = inputBox.value;
-  if (
-    // inputNumber.match(/^(?<halfWidthDigits>-?[0-9]+(\.?[0-9]*))$/) &&
-    inputNumber.match(validationTerms.digits.pattern) &&
-    !inputNumber.includes("e")
-  ) {
+  if (inputNumber.match(halfWidthDigits)) {
     validInput();
   } else {
     invalidInput();
