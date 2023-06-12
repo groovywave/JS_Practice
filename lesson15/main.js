5d54a974789ccf2212734aa669c4e3c3095f87b3
groove <vacation555@gmail.com>
2023-06-12 20:31:05 +0900

    delete changes of lesson14

--- lesson15/main.js
+++ lesson15/main.js
@@ -8,30 +8,24 @@ const modal = document.getElementById("js-modal");
 const mask = document.getElementById("js-mask");
 const promptMessage = document.getElementById("js-prompt-message");
 const inputBox = document.getElementById("js-input-box");
-const validationTerms = {
-  name: {
-    halfWidthDigits,
-  },
-  digits: {
-    pattern: /^(?<halfWidthDigits>-?[0-9]+(\.?[0-9]*))$/,
-  },
-};
-// console.log("test");
-// console.log(validationTerms.digits);
+const halfWidthDigits = /^-?\d+(\.?\d*)([eE][+-]?\d+)?$/;
 const url = "https://mocki.io/v1/1c058349-634e-462a-ad37-14f135e59b99";
 // const url = ""; //Not JSON
 // const url = "https://mocki.io/v1/55dc6233-a8fe-44ca-8906-3de313545ce8"; //No data
 // const url = "https://mocki.io/v1/1c058349-634e-"; //Failed to fetch
+
 function renderStatus(response) {
   errorMessage.id = "render-status";
   errorMessage.textContent = `${response.status}:${response.statusText}`;
   document.body.appendChild(errorMessage);
 }
+
 function displayInfo(error) {
   errorMessage.id = "display-info";
   errorMessage.textContent = error;
   document.body.appendChild(errorMessage);
 }
+
 function renderCircle() {
   const loadingCircle = document.createElement("img");
   loadingCircle.src = "img/loading-circle.gif";
@@ -39,9 +33,11 @@ function renderCircle() {
   loadingCircle.id = "loading-circle";
   ul.appendChild(loadingCircle);
 }
+
 function removeCircle() {
   document.getElementById("loading-circle").remove();
 }
+
 function renderData(menus) {
   const fragment = document.createDocumentFragment();
   fragment.id = "fragment";
@@ -60,6 +56,7 @@ function renderData(menus) {
   }
   ul.appendChild(fragment);
 }
+
 async function fetchData(url) {
   renderCircle();
   try {
@@ -80,6 +77,7 @@ async function fetchData(url) {
     backButton.classList.remove("hidden");
   }
 }
+
 async function fetchRenderData(inputNumber) {
   const responseData = await fetchData(url);
   if (responseData) {
@@ -87,6 +85,7 @@ async function fetchRenderData(inputNumber) {
     console.log(inputNumber); //show inputted number
   }
 }
+
 openButton.addEventListener("click", () => {
   promptMessage.textContent = "入力後ボタンを押してください";
   promptMessage.style.color = "black";
@@ -99,6 +98,7 @@ openButton.addEventListener("click", () => {
   openButton.classList.add("hidden");
   fetchButton.setAttribute("disabled", "true");
 });
+
 fetchButton.addEventListener("click", () => {
   if (fetchButton.hasAttribute("disabled")) {
     return;
@@ -108,17 +108,21 @@ fetchButton.addEventListener("click", () => {
   modal.classList.add("hidden");
   mask.classList.add("hidden");
 });
+
 function closeModal() {
   modal.classList.add("hidden");
   mask.classList.add("hidden");
   openButton.classList.remove("hidden");
 }
+
 mask.addEventListener("click", () => {
   closeModal();
 });
+
 closeButton.addEventListener("click", () => {
   closeModal();
 });
+
 backButton.addEventListener("click", () => {
   backButton.classList.add("hidden");
   openButton.classList.remove("hidden");
@@ -129,25 +133,25 @@ backButton.addEventListener("click", () => {
     ul.firstChild.remove();
   }
 });
+
 function checkInput() {
   const inputNumber = inputBox.value;
-  if (
-    // inputNumber.match(/^(?<halfWidthDigits>-?[0-9]+(\.?[0-9]*))$/) &&
-    inputNumber.match(validationTerms.digits.pattern) &&
-    !inputNumber.includes("e")
-  ) {
+  if (inputNumber.match(halfWidthDigits)) {
     validInput();
   } else {
     invalidInput();
   }
 }
+
 function validInput() {
   promptMessage.textContent = "";
   fetchButton.disabled = false;
 }
+
 function invalidInput() {
   promptMessage.textContent = "半角数値を入力ください";
   promptMessage.style.color = "red";
   fetchButton.disabled = true;
 }
+
 inputBox.addEventListener("input", checkInput);
