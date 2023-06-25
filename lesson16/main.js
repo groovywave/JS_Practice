1e9cde7530500bcc37fbb5603c00c9d849c3e93d
groove <vacation555@gmail.com>
2023-06-26 00:55:36 +0900

    make tabs

--- lesson16/main.js
+++ lesson16/main.js
@@ -1,11 +1,119 @@
 // import { format, differenceInCalendarDays } from "date-fns";
-// const tabMenuList = document.getElementById("js-tab-menu__list");
-const tabArea = getElementById("js-ul");
-const menuItems = document.querySelectorAll(".menu li a");
+// const tabMenuList = document.getElementById("js-tab-data__tabListst");
+// const articlesAPI = {
+//   main: " https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
+//   economy: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
+//   entertainment: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
+//   sports: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
+// };
+
+const articlesAPI = [
+  //main
+  "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
+  //economy
+  "https://mocki.io/v1/d408a3b3-9504-4c43-bf12-6773efb74361",
+  //entertainment
+  "https://mocki.io/v1/7b94b922-6130-44a6-b0c0-179cf5f76af5",
+  //sports
+  "https://mocki.io/v1/e30aa30c-649c-49ce-9d75-a4e9c4caca51",
+];
+
+const url = "https://mocki.io/v1/1c058349-634e-462a-ad37-14f135e59b99";
+const errorMessage = document.createElement("p");
+
+const tabArea = document.getElementById("js-ul");
+const menuItems = document.querySelectorAll(".menu tabList a");
 const contents = document.querySelectorAll(".content");
+
+async function fetchRenderData() {
+  const responseData = await Promise.all(articlesAPI.map(fetchData));
+  // const responseData = await fetchData(articlesAPI);
+  console.log(responseData);
+  if (responseData) {
+    renderData(responseData);
+  }
+}
+
+async function fetchData(url) {
+  renderCircle();
+  try {
+    const response = await fetch(url);
+    const responseData = await response.json();
+    if (!response.ok) {
+      renderStatus(response);
+      console.error(`${response.status}:${response.statusText}`);
+    }
+    if (!responseData.length) {
+      displayInfo("no data");
+    }
+    return responseData;
+  } catch (error) {
+    displayInfo(error);
+  } finally {
+    removeCircle();
+  }
+}
+
+function renderCircle() {
+  const loadingCircle = document.createElement("img");
+  loadingCircle.src = "img/loading-circle.gif";
+  loadingCircle.alt = "ローディング画像";
+  loadingCircle.id = "loading-circle";
+  tabArea.appendChild(loadingCircle);
+}
+
+function renderStatus(response) {
+  errorMessage.id = "render-status";
+  errorMessage.textContent = `${response.status}:${response.statusText}`;
+  document.body.appendChild(errorMessage);
+}
+
+function displayInfo(error) {
+  errorMessage.id = "display-info";
+  errorMessage.textContent = error;
+  document.body.appendChild(errorMessage);
+}
+
+function renderData(responseData) {
+  const fragmentTabs = document.createDocumentFragment();
+  for (const data of responseData) {
+    console.log(data.category);
+    const tabList = document.createElement("li");
+    const tabAnchor = document.createElement("a");
+    const img = document.createElement("img");
+    const section = document.createElement("section");
+    tabAnchor.href = "#";
+    console.log(tabAnchor.href);
+    tabAnchor.textContent = data.category;
+    console.log(tabAnchor.textContent);
+    tabAnchor.setAttribute("data-id", data.id);
+    console.log(tabAnchor.dataset.id);
+    img.src = data.image;
+    console.log(data.image);
+    img.alt = data.category;
+    fragmentTabs
+      .appendChild(tabList)
+      .appendChild(tabAnchor)
+      .insertAdjacentElement("afterbegin", img);
+  }
+  tabArea.appendChild(fragmentTabs);
+
+  const fragmentArticles = document.createDocumentFragment();
+  for (data of responseData) {
+    const section = document.createElement("section");
+    fragmentArticles.appendChild(section);
+  }
+}
+
+function removeCircle() {
+  document.getElementById("loading-circle").remove();
+}
+
+fetchRenderData();
 
 menuItems.forEach((clickedItem) => {
   clickedItem.addEventListener("click", (e) => {
+    console.log(clickedItem);
     console.log(e);
     e.preventDefault();
     //a タグの規定の動作（リンク先にページを遷移する）を無効化
