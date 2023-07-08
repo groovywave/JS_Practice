c040a84f1b3249006a8e5cca44dc7e60961191f3
groove <vacation555@gmail.com>
2023-07-08 17:18:04 +0900

    delete unnecessary code

--- lesson16/main.js
+++ lesson16/main.js
@@ -41,6 +41,14 @@ async function fetchData(url) {
   }
 }
 
+function renderCircle() {
+  const loadingCircle = document.createElement("img");
+  loadingCircle.src = "img/loading-circle.gif";
+  loadingCircle.alt = "ローディング画像";
+  loadingCircle.id = "loading-circle";
+  ul.appendChild(loadingCircle);
+}
+
 function renderStatus(response) {
   errorMessage.id = "render-status";
   errorMessage.textContent = `${response.status}:${response.statusText}`;
@@ -53,140 +61,6 @@ function displayInfo(error) {
   document.body.appendChild(errorMessage);
 }
 
-function renderCircle() {
-  const loadingCircle = document.createElement("img");
-  loadingCircle.src = "img/loading-circle.gif";
-  loadingCircle.alt = "ローディング画像";
-  loadingCircle.id = "loading-circle";
-  ul.appendChild(loadingCircle);
-}
-
 function removeCircle() {
   document.getElementById("loading-circle").remove();
 }
-
-function renderData(menus) {
-  const fragment = document.createDocumentFragment();
-  for (const menu of menus) {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const img = document.createElement("img");
-    a.href = menu.to;
-    a.textContent = menu.text;
-    img.src = menu.img;
-    img.alt = menu.alt;
-    fragment
-      .appendChild(li)
-      .appendChild(a)
-      .insertAdjacentElement("afterbegin", img);
-  }
-  ul.appendChild(fragment);
-}
-
-openButton.addEventListener("click", () => {
-  resetPrompt();
-  nameBox.value = "";
-  numberBox.value = "";
-  modal.classList.remove("hidden");
-  mask.classList.remove("hidden");
-  openButton.classList.add("hidden");
-  fetchButton.setAttribute("disabled", "true");
-  nameBox.focus();
-});
-
-fetchButton.addEventListener("click", () => {
-  const inputName = nameBox.value;
-  const inputNumber = numberBox.value;
-  fetchRenderData(inputName, inputNumber);
-  modal.classList.add("hidden");
-  mask.classList.add("hidden");
-});
-
-function closeModal() {
-  modal.classList.add("hidden");
-  mask.classList.add("hidden");
-  openButton.classList.remove("hidden");
-}
-
-mask.addEventListener("click", () => {
-  closeModal();
-  resetValidation();
-});
-
-closeButton.addEventListener("click", () => {
-  closeModal();
-  resetValidation();
-});
-
-backButton.addEventListener("click", () => {
-  backButton.classList.add("hidden");
-  openButton.classList.remove("hidden");
-  if (errorMessage) {
-    errorMessage.remove();
-  }
-  while (ul.firstChild) {
-    ul.firstChild.remove();
-  }
-});
-
-let isValidateName = false;
-let isValidateNumber = false;
-
-function validatePattern(inputBox, validPattern, errorMessage) {
-  const isValue = checkInputValue(inputBox, validPattern, errorMessage);
-  if (inputBox === nameBox) {
-    isValidateName = isValue;
-  } else if (inputBox === numberBox) {
-    isValidateNumber = isValue;
-  }
-  if (isValue) {
-    resetPrompt();
-    checkEnableSubmit();
-  }
-}
-
-function checkInputValue(inputBox, regExp, errorMessage) {
-  const value = inputBox.value;
-  const result = regExp.test(value);
-  if (!result) {
-    invalidInput(errorMessage);
-  }
-  return result;
-}
-
-function resetPrompt() {
-  promptMessage.textContent = "入力後、取得ボタンを押してね";
-  promptMessage.style.color = "black";
-}
-
-function resetValidation() {
-  isValidateName = false;
-  isValidateNumber = false;
-}
-
-function checkEnableSubmit() {
-  if (isValidateName && isValidateNumber) {
-    validInput();
-  } else {
-    invalidInput();
-  }
-}
-
-function invalidInput(errorMessage) {
-  promptMessage.textContent = errorMessage;
-  promptMessage.style.color = "red";
-  fetchButton.disabled = true;
-}
-
-function validInput() {
-  promptMessage.textContent = "入力後、取得ボタンを押してね";
-  promptMessage.style.color = "black";
-  fetchButton.disabled = false;
-}
-
-nameBox.addEventListener("input", () =>
-  validatePattern(nameBox, namePattern, "名前を入力ください")
-);
-numberBox.addEventListener("input", () =>
-  validatePattern(numberBox, numberPattern, "半角数字を入力ください")
-);
