4663eab0479b28c4a4bcdd3ddadc8d7cb7a51f1c
groove <vacation555@gmail.com>
2023-07-08 17:01:53 +0900

    make some json files

--- lesson16/main.js
+++ lesson16/main.js
@@ -1,33 +1,60 @@
-
-// const articlesAPI = {
-//   main: " https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
-//   economy: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
-//   entertainment: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
-//   sports: "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
-// };
-
-const articlesAPI = [
-  //main
-  "https://mocki.io/v1/025fa3d8-7096-433d-8766-8392ceab92b1",
-  //economy
-  "https://mocki.io/v1/d408a3b3-9504-4c43-bf12-6773efb74361",
-  //entertainment
-  "https://mocki.io/v1/7b94b922-6130-44a6-b0c0-179cf5f76af5",
-  //sports
-  "https://mocki.io/v1/e30aa30c-649c-49ce-9d75-a4e9c4caca51",
-];
-
-// const url = "https://mocki.io/v1/1c058349-634e-462a-ad37-14f135e59b99";
+const ul = document.getElementById("js-ul");
+const fetchButton = document.getElementById("js-fetch-button");
 const errorMessage = document.createElement("p");
-const tabArea = document.getElementById("js-ul");
-const articleArea = document.createElement("div");
+const openButton = document.getElementById("js-open-button");
+const closeButton = document.getElementById("js-close-button");
+const backButton = document.getElementById("js-back-button");
+const modal = document.getElementById("js-modal");
+const mask = document.getElementById("js-mask");
+const promptMessage = document.getElementById("js-prompt-message");
+const nameBox = document.getElementById("js-name-box");
+const numberBox = document.getElementById("js-number-box");
+const namePattern =
+  /^[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠a-zA-Zａ-ｚＡ-Ｚ]+[ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠a-zA-Zａ-ｚＡ-Ｚ\s]*$/;
+//https://arc-tech.hatenablog.com/entry/2021/01/20/105620
+const numberPattern = /^-?\d+(\.?\d*)([eE][+-]?\d+)?$/;
+const url = "https://mocki.io/v1/1c058349-634e-462a-ad37-14f135e59b99";
 
-async function fetchRenderData() {
-  const responseData = await Promise.all(articlesAPI.map(fetchData));
-  console.log(responseData);
-  if (responseData) {
-    renderData(responseData);
+function renderStatus(response) {
+  errorMessage.id = "render-status";
+  errorMessage.textContent = `${response.status}:${response.statusText}`;
+  document.body.appendChild(errorMessage);
+}
+
+function displayInfo(error) {
+  errorMessage.id = "display-info";
+  errorMessage.textContent = error;
+  document.body.appendChild(errorMessage);
+}
+
+function renderCircle() {
+  const loadingCircle = document.createElement("img");
+  loadingCircle.src = "img/loading-circle.gif";
+  loadingCircle.alt = "ローディング画像";
+  loadingCircle.id = "loading-circle";
+  ul.appendChild(loadingCircle);
+}
+
+function removeCircle() {
+  document.getElementById("loading-circle").remove();
+}
+
+function renderData(menus) {
+  const fragment = document.createDocumentFragment();
+  for (const menu of menus) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    a.href = menu.to;
+    a.textContent = menu.text;
+    img.src = menu.img;
+    img.alt = menu.alt;
+    fragment
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin", img);
   }
+  ul.appendChild(fragment);
 }
 
 async function fetchData(url) {
@@ -47,133 +74,124 @@ async function fetchData(url) {
     displayInfo(error);
   } finally {
     removeCircle();
+    backButton.classList.remove("hidden");
   }
 }
 
-function renderCircle() {
-  const loadingCircle = document.createElement("img");
-  loadingCircle.src = "img/loading-circle.gif";
-  loadingCircle.alt = "ローディング画像";
-  loadingCircle.id = "loading-circle";
-  tabArea.appendChild(loadingCircle);
-}
-
-function renderStatus(response) {
-  errorMessage.id = "render-status";
-  errorMessage.textContent = `${response.status}:${response.statusText}`;
-  document.body.appendChild(errorMessage);
-}
-
-function displayInfo(error) {
-  errorMessage.id = "display-info";
-  errorMessage.textContent = error;
-  document.body.appendChild(errorMessage);
-}
-
-const fragmentTabs = document.createDocumentFragment();
-const fragmentTitles = document.createDocumentFragment();
-const fragmentGenres = document.createDocumentFragment();
-
-function renderData(responseData) {
-  for (const data of responseData) {
-    //Tabsの生成
-    const tabTitle = document.createElement("li");
-    const tabAnchor = document.createElement("a");
-    tabAnchor.href = "#";
-    tabAnchor.textContent = data.category;
-    tabAnchor.setAttribute("data-id", data.id);
-    tabAnchor.classList.add("tab");
-    if (data.select) {
-      tabAnchor.classList.add("active");
-    }
-    fragmentTabs.appendChild(tabTitle).appendChild(tabAnchor);
-
-    //記事タイトルの作成
-    const articleList = data.article;
-    for (const article of articleList) {
-      const title = document.createElement("li");
-      const titleAnchor = document.createElement("a");
-      titleAnchor.href = "#";
-      titleAnchor.textContent = article.title;
-      console.log(article.date);
-      const articleDate = new Date(article.date);
-      if (withinThreeDays(articleDate)) {
-        const newIcon = document.createElement("img");
-        newIcon.src = "./img/new.png";
-        // title.classList.add("new");
-        newIcon.classList.add("new");
-        titleAnchor.appendChild(newIcon);
-        // titleAnchor.appendChild(addNew);
-        // titleAnchor.insertAdjacentElement("afterbegin", addNew);
-      }
-      fragmentTitles.appendChild(title).appendChild(titleAnchor);
-    }
-
-    //imageの作成
-    const img = document.createElement("img");
-    img.src = data.image;
-    img.width = "100";
-    img.height = "100";
-
-    // const genre = document.createElement("section");
-    const genreContainer = document.createElement("div");
-    genreContainer.classList.add("genre-container");
-    const titleArea = document.createElement("div");
-    const imageArea = document.createElement("div");
-    titleArea.classList.add("content", "title-area");
-    imageArea.classList.add("content", "image-area");
-    genreContainer.id = data.id;
-    // genre.classList.add("genre-container");
-    genreContainer.appendChild(titleArea).appendChild(fragmentTitles);
-    genreContainer.appendChild(imageArea).appendChild(img);
-    //初期表示の設定
-    if (data.select) {
-      genreContainer.classList.add("active");
-    }
-    fragmentGenres.appendChild(genreContainer);
+async function fetchRenderData(inputNumber) {
+  const responseData = await fetchData(url);
+  if (responseData) {
+    renderData(responseData);
+    console.log(inputNumber); //show inputted number
   }
-  tabArea.appendChild(fragmentTabs);
-  articleArea.appendChild(fragmentGenres);
-  console.log(articleArea);
-  tabArea.insertAdjacentElement("afterend", articleArea);
-  const tabs = Array.from(document.getElementsByClassName("tab"));
-  const contents = Array.from(
-    document.getElementsByClassName("genre-container")
-  );
-  addClickEvent(tabs, contents);
 }
 
-function removeCircle() {
-  document.getElementById("loading-circle").remove();
+openButton.addEventListener("click", () => {
+  resetPrompt();
+  nameBox.value = "";
+  numberBox.value = "";
+  modal.classList.remove("hidden");
+  mask.classList.remove("hidden");
+  openButton.classList.add("hidden");
+  fetchButton.setAttribute("disabled", "true");
+  nameBox.focus();
+});
+
+fetchButton.addEventListener("click", () => {
+  const inputName = nameBox.value;
+  const inputNumber = numberBox.value;
+  fetchRenderData(inputName, inputNumber);
+  modal.classList.add("hidden");
+  mask.classList.add("hidden");
+});
+
+function closeModal() {
+  modal.classList.add("hidden");
+  mask.classList.add("hidden");
+  openButton.classList.remove("hidden");
 }
 
-fetchRenderData();
+mask.addEventListener("click", () => {
+  closeModal();
+  resetValidation();
+});
 
-function withinThreeDays(day) {
-  const today = new Date();
-  console.log(today);
-  const msInThreeDays = 3 * 24 * 60 * 60 * 1000;
-  const diff = today.getTime() - day.getTime();
-  console.log(diff);
-  if (diff < msInThreeDays) {
-    return true;
+closeButton.addEventListener("click", () => {
+  closeModal();
+  resetValidation();
+});
+
+backButton.addEventListener("click", () => {
+  backButton.classList.add("hidden");
+  openButton.classList.remove("hidden");
+  if (errorMessage) {
+    errorMessage.remove();
+  }
+  while (ul.firstChild) {
+    ul.firstChild.remove();
+  }
+});
+
+let isValidateName = false;
+let isValidateNumber = false;
+
+function validatePattern(inputBox, validPattern, errorMessage) {
+  const isValue = checkInputValue(inputBox, validPattern, errorMessage);
+  if (inputBox === nameBox) {
+    isValidateName = isValue;
+  } else if (inputBox === numberBox) {
+    isValidateNumber = isValue;
+  }
+  if (isValue) {
+    resetPrompt();
+    checkEnableSubmit();
+  }
+}
+
+function checkInputValue(inputBox, regExp, errorMessage) {
+  const value = inputBox.value;
+  const result = regExp.test(value);
+  if (!result) {
+    invalidInput(errorMessage);
+  }
+  return result;
+}
+
+function resetPrompt() {
+  promptMessage.textContent = "入力後、取得ボタンを押してね";
+  promptMessage.style.color = "black";
+}
+
+function resetValidation() {
+  isValidateName = false;
+  isValidateNumber = false;
+}
+
+function checkEnableSubmit() {
+  if (isValidateName && isValidateNumber) {
+    validInput();
   } else {
-    return false;
+    invalidInput();
   }
 }
 
-function addClickEvent(elements, contents) {
-  elements.forEach((clickedItem) => {
-    clickedItem.addEventListener("click", (e) => {
-      e.preventDefault();
-      elements.forEach((item) => {
-        item.classList.remove("active");
-      });
-      clickedItem.classList.add("active");
-      contents.forEach((genreContainer) => {
-        genreContainer.classList.remove("active");
-      });
-      document.getElementById(clickedItem.dataset.id).classList.add("active");
-    });
-  });
+
+function invalidInput(errorMessage) {
+  promptMessage.textContent = errorMessage;
+  promptMessage.style.color = "red";
+  fetchButton.disabled = true;
 }
+
+function validInput() {
+  promptMessage.textContent = "入力後、取得ボタンを押してね";
+  promptMessage.style.color = "black";
+  fetchButton.disabled = false;
+}
+
+nameBox.addEventListener("input", () =>
+  validatePattern(nameBox, namePattern, "名前を入力ください")
+);
+numberBox.addEventListener("input", () =>
+  validatePattern(numberBox, numberPattern, "半角数字を入力ください")
+);
+
