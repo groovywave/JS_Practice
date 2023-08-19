c3771a7a16e328ff14d8912c0789a231efc7aacb
groove <vacation555@gmail.com>
2023-08-19 16:17:26 +0900

    add button and clickEventListener on the button

--- lesson17/main.js
+++ lesson17/main.js
@@ -1,6 +1,7 @@
 const slides = [];
 const images = [];
 const url = "https://mocki.io/v1/010175de-0176-440a-9f90-d4a7ca8010cc";
+let currentIndex;
 
 function renderStatus(response) {
   const errorMessage = document.createElement("p");
@@ -14,8 +15,19 @@ function displayInfo(error) {
   document.body.appendChild(errorMessage);
 }
 
-function slidesMovePrev() {}
-function slidesMoveNext() {}
+function slidesMovePrev() {
+  for (const slide of slides) {
+    const slideZIndex = parseInt(slide.style.zIndex, 10);
+    slide.style.zIndex = (slideZIndex + slides.length - 1) % slides.length;
+  }
+}
+
+function slidesMoveNext() {
+  for (const slide of slides) {
+    const slideZIndex = parseInt(slide.style.zIndex, 10);
+    slide.style.zIndex = (slideZIndex + 1) % slides.length;
+  }
+}
 
 function renderData(images) {
   const fragment = document.createDocumentFragment();
@@ -26,31 +38,33 @@ function renderData(images) {
   carouselContainer.className = "carousel-container";
   let i = 0;
   for (const image of images) {
-    const img = document.createElement("img");
-    img.className = "carousel-img";
-    img.src = image.img;
-    img.alt = image.alt;
+    const slide = document.createElement("img");
+    slide.className = "carousel-img";
+    slide.src = image.img;
+    slide.alt = image.alt;
     i = --i;
-    img.style.zIndex = images.length + i;
-    fragment.appendChild(img);
+    slide.style.zIndex = images.length + i;
+    fragment.appendChild(slide);
+    slides.push(slide);
   }
+  currentIndex = images.length - 1;
 
   const prevButton = document.createElement("button");
   prevButton.id = "prev";
   prevButton.style.zIndex = 100;
-  const prevIcon = document.createElement("i")
-  prevIcon.className="fa-solid fa-backward"
-  prevButton.addEventListener("click", slidesMovePrev());
+  const prevIcon = document.createElement("i");
+  prevIcon.className = "fa-solid fa-backward";
+  prevButton.addEventListener("click", slidesMovePrev);
   fragment.appendChild(prevButton).appendChild(prevIcon);
-  
+
   const nextButton = document.createElement("button");
   nextButton.id = "next";
   nextButton.style.zIndex = 100;
-  const nextIcon = document.createElement("i")
-  nextIcon.className="fa-solid fa-forward"
-  prevButton.addEventListener("click", slidesMoveNext());
+  const nextIcon = document.createElement("i");
+  nextIcon.className = "fa-solid fa-forward";
+  nextButton.addEventListener("click", slidesMoveNext);
   fragment.appendChild(nextButton).appendChild(nextIcon);
-  
+
   document.body
     .appendChild(carousel)
     .appendChild(carouselContainer)
