d741e9f44fa58c961d6a41acac7f7a074ed0416e
groove <vacation555@gmail.com>
2023-08-23 14:36:46 +0900

    add element p as slidesNumber and function updateSlidesNumber

--- lesson17/main.js
+++ lesson17/main.js
@@ -17,13 +17,19 @@ function getZIndex(slide) {
   return parseInt(slide.style.zIndex, 10);
 }
 
+function getCurrentSlide(slides) {
+  return slides.find((slide) => getZIndex(slide) === slides.length - 1);
+}
+
 function updateButton(slides) {
-  const currentSlide = slides.find(
-    (slide) => getZIndex(slide) === slides.length - 1
-  );
-  currentSlide.parentNode.querySelector(".js-current").classList.remove("js-current");
+  const currentSlide = getCurrentSlide(slides);
+  currentSlide.parentNode
+    .querySelector(".js-current")
+    .classList.remove("js-current");
   currentSlide.classList.add("js-current");
-  currentSlide.parentNode.querySelector(".js-hidden")?.classList.remove("js-hidden");
+  currentSlide.parentNode
+    .querySelector(".js-hidden")
+    ?.classList.remove("js-hidden");
   if (currentSlide === slides[0]) {
     document.getElementById("prev").classList.add("js-hidden");
   }
@@ -32,12 +38,20 @@ function updateButton(slides) {
   }
 }
 
+function updateSlidesNumber(slides) {
+  const currentSlide = getCurrentSlide(slides);
+  slidesNumber.textContent = `${slides.indexOf(currentSlide) + 1}/${
+    slides.length
+  }`;
+}
+
 function slidesMovePrev() {
   for (const slide of slides) {
     const slideZIndex = getZIndex(slide);
     slide.style.zIndex = (slideZIndex + slides.length - 1) % slides.length;
   }
   updateButton(slides);
+  updateSlidesNumber(slides);
 }
 
 function slidesMoveNext() {
@@ -46,23 +60,24 @@ function slidesMoveNext() {
     slide.style.zIndex = (slideZIndex + 1) % slides.length;
   }
   updateButton(slides);
+  updateSlidesNumber(slides);
 }
 
 function renderData(images) {
   const fragment = document.createDocumentFragment();
   const carousel = document.createElement("section");
   carousel.id = "carousel";
-  const carouselContainer = document.createElement("div");
-  carouselContainer.id = "carouselContainer";
-  carouselContainer.className = "js-carousel-container";
-  let i = 0;
-  for (const image of images) {
+  const slidesContainer = document.createElement("div");
+  slidesContainer.id = "carouselContainer";
+  slidesContainer.className = "js-slides-container";
+  for (const [index, image] of images.entries()) {
     const slide = document.createElement("img");
     slide.className = "js-carousel-img";
     slide.src = image.img;
     slide.alt = image.alt;
-    --i;
-    slide.style.zIndex = images.length + i;
+    slide.index = index;
+    console.log(slide.index);
+    slide.style.zIndex = images.length - 1 - slide.index;
     fragment.appendChild(slide);
     slides.push(slide);
   }
@@ -86,8 +101,15 @@ function renderData(images) {
 
   document.body
     .appendChild(carousel)
-    .appendChild(carouselContainer)
+    .appendChild(slidesContainer)
     .appendChild(fragment);
+
+  const slidesNumberContainer = document.createElement("div");
+  slidesNumberContainer.id = "slidesNumberContainer";
+  const slidesNumber = document.createElement("p");
+  slidesNumber.id = "slidesNumber";
+  slidesNumber.textContent = `${slides[0].index + 1}/${slides.length}`;
+  carousel.appendChild(slidesNumber);
 
   updateButton(slides);
 }
