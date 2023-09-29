aea2198fececec48c467b69a77a0ea4b4b9e6448
groove <vacation555@gmail.com>
2023-09-29 12:29:21 +0900

    Revert "feat:fetch twice for header and body"
    This reverts commit 4d01d4bfa317258e033620ba17d2cd4c818da636.

--- src/lesson20/contents/userTable.js
+++ src/lesson20/contents/userTable.js
@@ -3,10 +3,7 @@ import { removeCircle } from "../modules/removeCircle.js";
 import { displayInfo } from "../modules/displayInfo.js";
 
 // const url = "https://mocki.io/v1/d8444d45-bf2c-4980-9a81-6d2bbd02dd6e";
-// const url = "https://mocki.io/v1/55433c48-118c-4b6d-bd79-6db5e34ceed9";
-const urlForHead = "https://mocki.io/v1/c7710cdf-1d83-4086-90f5-2d39332739bc";
-const urlForBody = "https://mocki.io/v1/e8d754fc-e71a-4445-9258-44d03be4fbec";
-const urls = [urlForHead, urlForBody];
+const url = "https://mocki.io/v1/55433c48-118c-4b6d-bd79-6db5e34ceed9";
 
 function renderStatus(response) {
   const errorMessage = document.createElement("p");
@@ -32,16 +29,16 @@ function makeRow(theadOrTbody, thOrTd, dataSet) {
   return fragment;
 }
 
-const tableContainer = document.createElement("div");
-tableContainer.classList.add("table-container");
-const table = document.createElement("table");
-table.classList.add("table");
-
 function makeTable(dataSet) {
-  // let headerData = [];
-  // headerData.push(dataSet[0]);
+  const tableContainer = document.createElement("div");
+  tableContainer.classList.add("table-container");
+  const table = document.createElement("table");
+  table.classList.add("table");
 
-  // const slicedDataSet = dataSet.slice(1);
+  let headerData = [];
+  headerData.push(dataSet[0]);
+
+  const slicedDataSet = dataSet.slice(1);
   makeRow("thead", "th", headerData);
   makeRow("tbody", "td", slicedDataSet);
 
@@ -77,24 +74,15 @@ async function fetchData(url) {
   }
 }
 
-// [urlForHead, urlForBody].forEach(() => {
 async function fetchMakeTable() {
   const responseData = await fetchData(url);
   if (responseData) {
     makeTable(responseData);
   }
 }
-// });
 
 async function init() {
   await fetchMakeTable();
 }
 
 init();
-
-// const promisedDataSet = urls.map((url) => fetchData(url));
-// const rowDataSet = await Promise.allSettled(promisedDataSet);
-// removeCircle();
-// return rowDataSet
-//   .filter((data) => data.status === "fulfilled")
-//   .map((data) => data.value);
