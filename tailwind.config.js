1aa23ed72343b7ac6e51d1a2be13c1a0ea435a45
groove <vacation555@gmail.com>
2023-12-10 20:32:59 +0900

    use tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        successColor: "#2ecc71",
+        errorColor: "#e74c3c",
+      },
+    },
+  },
+  plugins: [],
+};
