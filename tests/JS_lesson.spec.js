5a56b6cea392b5c751a1955f0c896fc092d60796
groove <vacation555@gmail.com>
2024-01-07 00:57:10 +0900

    feat:fix test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -5,56 +5,10 @@ test("test", async ({ page }) => {
   const takeScreenshot = async (name) => {
     await page.screenshot({ path: `screenshot/${name}.png` });
   };
-  await page.goto(
-    "https://stackblitz.com/edit/stackblitz-starters-axeudy?file=README.md"
-  );
-  await page.goto("https://stackblitz.com/edit/stackblitz-starters-axeudy");
-  await page.goto(
-    "https://stackblitz.com/edit/stackblitz-starters-axeudy?file=README.md"
-  );
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByRole("link", { name: "lesson24" })
-    .click();
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByPlaceholder("Enter username")
-    .press("Tab");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByPlaceholder("Enter email")
-    .press("Tab");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByPlaceholder("Enter password", { exact: true })
-    .press("Tab");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByPlaceholder("Enter password again")
-    .press("Tab");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByRole("link", { name: "利用規約" })
-    .press("Enter");
-  await takeScreenshot("click 利用規約");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByText("利用規約を読み、同意しました", { exact: true })
-    .click();
+  await page.goto("http://127.0.0.1:4173/");
+  await page.getByRole("link", { name: "lesson24" }).click();
+  await page.getByText("利用規約を読み、同意しました", { exact: true }).click();
   await takeScreenshot("click agreeButton");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByLabel("利用規約を読み、同意しました。")
-    .uncheck();
-  await takeScreenshot("checkbox unchecked");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByLabel("利用規約を読み、同意しました。")
-    .check();
-  await takeScreenshot("checkbox checked");
-  await page
-    .frameLocator('iframe[title="Preview page"]')
-    .getByRole("button", { name: "Submit" })
-    .click();
+  await page.getByRole("button", { name: "Submit" }).click();
   await takeScreenshot("click submitButton");
 });
