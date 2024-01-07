c504d2337bebca6a2ef5343fd36586f1e67efcff
groove <vacation555@gmail.com>
2024-01-07 23:23:21 +0900

    feat:add function keydownToScrollModal

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -1,12 +1,69 @@
 // @ts-check
-const { test, expect } = require("@playwright/test");
+
+import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
   const takeScreenshot = async (name) => {
     await page.screenshot({ path: `screenshot/${name}.png` });
   };
-  await page.goto("http://127.0.0.1:4173/");
+
+  await page.goto("http://127.0.0.1:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
+  await page.getByPlaceholder("Enter username").click();
+  await page.getByPlaceholder("Enter username").press("Tab");
+  await page.getByPlaceholder("Enter email").press("Tab");
+  await page.getByPlaceholder("Enter password", { exact: true }).press("Tab");
+  await page.getByPlaceholder("Enter password again").press("Tab");
+  await page.getByRole("link", { name: "利用規約" }).press("Enter");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
+  await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
   await page.getByText("利用規約を読み、同意しました", { exact: true }).click();
   await takeScreenshot("click agreeButton");
   await page.getByRole("button", { name: "Submit" }).click();
