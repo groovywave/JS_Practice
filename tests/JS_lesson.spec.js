06c0db1c92df2160530bb0b0ac2e5ae262432317
groove <vacation555@gmail.com>
2024-01-17 00:34:46 +0900

    feat:remove screenshot in test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -1,9 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("press tab-key and focus on 利用規約", async ({ page }) => {
-  const takeScreenshot = async (name) => {
-    await page.screenshot({ path: `screenshot/${name}.png` });
-  };
   await page.goto("localhost:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
   await page.getByPlaceholder("Enter username").click();
@@ -11,26 +8,18 @@ test("press tab-key and focus on 利用規約", async ({ page }) => {
   await page.getByPlaceholder("Enter email").press("Tab");
   await page.getByPlaceholder("Enter password", { exact: true }).press("Tab");
   await page.getByPlaceholder("Enter password again").press("Tab");
-  takeScreenshot("focus on 利用規約");
   await expect(page.getByRole("link", { name: "利用規約" })).toBeFocused();
 });
 
 test("press Enter on the link to rule-page", async ({ page }) => {
-  const takeScreenshot = async (name) => {
-    await page.screenshot({ path: `screenshot/${name}.png` });
-  };
   await page.goto("localhost:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
   await page.getByPlaceholder("Enter password again").click();
   await page.getByPlaceholder("Enter password again").press("Tab");
   await page.getByRole("link", { name: "利用規約" }).press("Enter");
-  await takeScreenshot("press Enter on the link that to go to rule page");
 });
 
 test("Click the 'Agree' button to go to the form page", async ({ page }) => {
-  const takeScreenshot = async (name) => {
-    await page.screenshot({ path: `screenshot/${name}.png` });
-  };
   await page.goto("localhost:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
   await page.getByPlaceholder("Enter password again").click();
@@ -39,16 +28,11 @@ test("Click the 'Agree' button to go to the form page", async ({ page }) => {
   await page.getByTestId("js-agreeButton").scrollIntoViewIfNeeded();
   await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
   await page.getByText("利用規約を読み、同意しました", { exact: true }).click();
-  await takeScreenshot("Click the 'Agree' button to go to the form page");
 });
 
 test("Click the 'Submit' button to go to the registration complete page", async ({
   page,
 }) => {
-  const takeScreenshot = async (name) => {
-    await page.screenshot({ path: `screenshot/${name}.png` });
-  };
-
   await page.goto("localhost:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
   await page.getByPlaceholder("Enter password again").click();
@@ -58,7 +42,4 @@ test("Click the 'Submit' button to go to the registration complete page", async 
   await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
   await page.getByText("利用規約を読み、同意しました", { exact: true }).click();
   await page.getByRole("button", { name: "Submit" }).click();
-  await takeScreenshot(
-    "Click the 'Submit' button to go to the registration complete page",
-  );
 });
