fc2e4d8dab41234ce87901bf28629042821678ba
groove <vacation555@gmail.com>
2024-01-17 18:56:09 +0900

    refactor:use beforeEach() in test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test";
-
-test("press tab-key and focus on 利用規約", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("localhost:3000/");
   await page.getByRole("link", { name: "lesson24" }).click();
   await page.getByPlaceholder("Enter username").click();
@@ -11,19 +10,13 @@ test("press tab-key and focus on 利用規約", async ({ page }) => {
   await expect(page.getByRole("link", { name: "利用規約" })).toBeFocused();
 });
 
+test("press tab-key and focus on 利用規約", async ({ page }) => {});
+
 test("press Enter on the link to rule-page", async ({ page }) => {
-  await page.goto("localhost:3000/");
-  await page.getByRole("link", { name: "lesson24" }).click();
-  await page.getByPlaceholder("Enter password again").click();
-  await page.getByPlaceholder("Enter password again").press("Tab");
   await page.getByRole("link", { name: "利用規約" }).press("Enter");
 });
 
 test("Click the 'Agree' button to go to the form page", async ({ page }) => {
-  await page.goto("localhost:3000/");
-  await page.getByRole("link", { name: "lesson24" }).click();
-  await page.getByPlaceholder("Enter password again").click();
-  await page.getByPlaceholder("Enter password again").press("Tab");
   await page.getByRole("link", { name: "利用規約" }).press("Enter");
   await page.getByTestId("js-agreeButton").scrollIntoViewIfNeeded();
   await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
@@ -33,10 +26,6 @@ test("Click the 'Agree' button to go to the form page", async ({ page }) => {
 test("Click the 'Submit' button to go to the registration complete page", async ({
   page,
 }) => {
-  await page.goto("localhost:3000/");
-  await page.getByRole("link", { name: "lesson24" }).click();
-  await page.getByPlaceholder("Enter password again").click();
-  await page.getByPlaceholder("Enter password again").press("Tab");
   await page.getByRole("link", { name: "利用規約" }).press("Enter");
   await page.getByTestId("js-agreeButton").scrollIntoViewIfNeeded();
   await page.getByLabel("利用規約", { exact: true }).press("ArrowDown");
