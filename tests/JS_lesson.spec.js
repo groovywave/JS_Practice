ba0e7935c8505b05d64eabf1e711a74ad929c05e
groove <vacation555@gmail.com>
2024-02-01 22:46:43 +0900

    feat:add test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -1,37 +1,85 @@
 import { test, expect } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:3000/lesson24/index.html');
+  await page.goto('http://localhost:3000/lesson25/index.html');
   await page.getByLabel('Username').click();
   await page.getByLabel('Username').press('Tab');
   await page.getByLabel('Email').press('Tab');
   await page.getByLabel('Password', { exact: true }).press('Tab');
   await page.getByLabel('Confirm Password').press('Tab');
-});
-
-test('press tab-key and focus on 利用規約', async ({ page }) => {
-  await expect(page.getByRole('link', { name: '利用規約' })).toBeFocused();
-});
-
-test('press Enter on the link and open the modal', async ({ page }) => {
-  await page.getByRole('link', { name: '利用規約' }).press('Enter');
-  await expect(page.getByTestId('js-modalBody')).toBeVisible();
-});
-
-test("Click the 'Agree' button to go to the form page", async ({ page }) => {
   await page.getByRole('link', { name: '利用規約' }).press('Enter');
   await page.getByTestId('js-agreeButton').scrollIntoViewIfNeeded();
   await page.getByLabel('利用規約', { exact: true }).press('ArrowDown');
-  await expect(page.getByLabel('Username')).toBeVisible();
   await page.getByText('利用規約を読み、同意しました', { exact: true }).click();
-  await expect(page.getByTestId('js-modalBody')).toBeHidden();
 });
 
-test("Click the 'Submit' button to go to the registration complete page", async ({
+test('input less than 3 characters into username and show error message', async ({
   page
 }) => {
-  await page.getByRole('link', { name: '利用規約' }).press('Enter');
-  await page.getByTestId('js-agreeButton').scrollIntoViewIfNeeded();
-  await page.getByText('利用規約を読み、同意しました', { exact: true }).click();
+  await page.getByLabel('Username').fill('e');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(
+    page.getByTestId('js-usernameError', {
+      name: 'Username must be at least 3 characters'
+    })
+  ).toBeVisible();
+});
+
+test('input more than 15 characters into username and show error message', async ({
+  page
+}) => {
+  await page.getByLabel('Username').fill('01234567890123456789');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(
+    page.getByTestId('js-usernameError', {
+      name: 'Username must be less than 15 characters'
+    })
+  ).toBeVisible();
+});
+
+test('input invalid mail address into email and show error message', async ({
+  page
+}) => {
+  await page.getByLabel('Email').fill('invalid email');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(
+    page.getByTestId('js-emailError', {
+      name: 'Email is not valid'
+    })
+  ).toBeVisible();
+});
+
+test('input less than 8 characters into Password and show error message', async ({
+  page
+}) => {
+  await page.getByTestId('js-passwordLabel').fill('abc');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(
+    page.getByTestId('js-passwordError', {
+      name: 'Must be alphanumeric and at least 8 characters long'
+    })
+  ).toBeVisible();
+});
+
+test('input wrong password into Confirm Password and show error message', async ({
+  page
+}) => {
+  await page.getByTestId('js-passwordLabel').fill('KevinDeBruyne');
+  await page.getByTestId('js-confirmPasswordLabel').fill('111');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(
+    page.getByTestId('js-confirmPasswordError', {
+      name: 'Passwords do not match'
+    })
+  ).toBeVisible();
+});
+
+test('Show a completion message after the form is successfully submitted', async ({
+  page
+}) => {
+  await page.getByLabel('Username').fill('Alexander');
+  await page.getByLabel('Email').fill('Arnord@liverpool.com');
+  await page.getByTestId('js-passwordLabel').fill('KevinDeBruyne');
+  await page.getByTestId('js-confirmPasswordLabel').fill('KevinDeBruyne');
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByRole('heading', {
