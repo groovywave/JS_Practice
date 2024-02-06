2b0ac2a9b08e3443e51a172a3d6155ac354a0162
groove <vacation555@gmail.com>
2024-02-06 21:19:21 +0900

    feat:fix test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/lesson25/index.html');
   await page.getByLabel('Username').click();
@@ -9,16 +10,13 @@ test.beforeEach(async ({ page }) => {
   await page.getByRole('link', { name: '利用規約' }).press('Enter');
   await page.getByTestId('js-lastSentence').scrollIntoViewIfNeeded();
   await page.getByTestId('js-lastSentence').press('ArrowDown');
-  // await page.getByLabel('利用規約', { exact: true }).press('ArrowDown');
   await page.getByTestId('js-closeButton').click();
-  // await page.getByText('利用規約を読み、同意しました', { exact: true }).click();
 });
 
 test('input less than 3 characters into username and show error message', async ({
   page
 }) => {
   await page.getByLabel('Username').fill('e');
-  await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByTestId('js-usernameError', {
       name: 'Username must be at least 3 characters'
@@ -30,7 +28,6 @@ test('input more than 15 characters into username and show error message', async
   page
 }) => {
   await page.getByLabel('Username').fill('01234567890123456789');
-  await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByTestId('js-usernameError', {
       name: 'Username must be less than 15 characters'
@@ -42,7 +39,6 @@ test('input invalid mail address into email and show error message', async ({
   page
 }) => {
   await page.getByLabel('Email').fill('invalid email');
-  await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByTestId('js-emailError', {
       name: 'Email is not valid'
@@ -54,7 +50,6 @@ test('input less than 8 characters into Password and show error message', async 
   page
 }) => {
   await page.getByTestId('js-passwordLabel').fill('abc');
-  await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByTestId('js-passwordError', {
       name: 'at least 8 characters that include at least one uppercase letter, one lowercase letter, and one digit'
@@ -67,7 +62,6 @@ test('input wrong password into Confirm Password and show error message', async 
 }) => {
   await page.getByTestId('js-passwordLabel').fill('KevinDeBruyne17');
   await page.getByTestId('js-confirmPasswordLabel').fill('KevinDeBruyne7');
-  await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
     page.getByTestId('js-confirmPasswordError', {
       name: 'Passwords do not match'
