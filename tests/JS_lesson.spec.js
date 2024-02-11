71044326a8efbde94f1ca3e1432005c50126d6e0
groove <vacation555@gmail.com>
2024-02-11 11:36:47 +0900

    feat:fix test code

--- tests/JS_lesson.spec.js
+++ tests/JS_lesson.spec.js
@@ -9,36 +9,14 @@ test.beforeEach(async ({ page }) => {
   await page.getByLabel('Confirm Password').press('Tab');
 });
 
-test('press Enter on the link and open the modal', async ({ page }) => {
-  await page.getByRole('link', { name: '利用規約' }).press('Enter');
-  await expect(page.getByTestId('js-modalBody')).toBeVisible();
-});
-
 test('press tab-key and focus on 利用規約', async ({ page }) => {
+  await page.getByRole('checkbox').press('Tab');
   await expect(page.getByRole('link', { name: '利用規約' })).toBeFocused();
 });
 
-test("Click the 'Agree' button to go to the form page", async ({ page }) => {
+test('press Enter on the link and open the modal', async ({ page }) => {
   await page.getByRole('link', { name: '利用規約' }).press('Enter');
-  await page.getByTestId('js-agreeButton').scrollIntoViewIfNeeded();
-  await page.getByLabel('利用規約', { exact: true }).press('ArrowDown');
-  await expect(page.getByLabel('Username')).toBeVisible();
-  await page.getByText('利用規約を読み、同意しました', { exact: true }).click();
-  await expect(page.getByTestId('js-modalBody')).toBeHidden();
-});
-
-test("Click the 'Submit' button to go to the registration complete page", async ({
-  page
-}) => {
-  await page.getByRole('link', { name: '利用規約' }).press('Enter');
-  await page.getByTestId('js-agreeButton').scrollIntoViewIfNeeded();
-  await page.getByText('利用規約を読み、同意しました', { exact: true }).click();
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(
-    page.getByRole('heading', {
-      name: 'Your Registration Has Been Completed!'
-    })
-  ).toBeVisible();
+  await expect(page.getByTestId('js-modalBody')).toBeVisible();
 });
 
 test.beforeEach(async ({ page }) => {
