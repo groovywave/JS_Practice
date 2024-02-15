af401e115cc91f326ecc8caca14603a123fb734f
groove <vacation555@gmail.com>
2024-02-15 23:55:32 +0900

    review the placement of functions

--- src/lesson25/js/main.js
+++ src/lesson25/js/main.js
@@ -1,94 +1,27 @@
+import * as validation from './modules/validation.js';
+
 const username = document.getElementById('username');
 username.focus();
-const linkToRule = document.getElementById('js-linkToRule');
-const mask = document.getElementById('js-mask');
-const modal = document.getElementById('js-modal');
-const modalBody = document.getElementById('js-modalBody');
-linkToRule.addEventListener('click', event => {
-  event.preventDefault();
-  mask.classList.remove('hidden');
-  modal.classList.remove('hidden');
-  modalBody.focus();
-});
-const submitCheckbox = document.getElementById('js-submitCheckbox');
-const submitButton = document.getElementById('js-submitButton');
-function checkboxToBeChecked() {
-  submitCheckbox.disabled = false;
-  submitCheckbox.checked = true;
-  if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
-  submitButton.disabled = false;
-}
 
-function closeModal() {
-  mask.classList.add('hidden');
-  modal.classList.add('hidden');
-}
-
-function addToggleToTheSubmitCheckbox() {
-  submitCheckbox.addEventListener('change', () => {
-    if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
-    if (!submitCheckbox.checked) {
-      submitButton.disabled = true;
-    } else {
-      submitButton.disabled = false;
-    }
-  });
-}
-mask.addEventListener('click', () => {
-  closeModal();
-  username.focus();
-});
-const closeButton = document.getElementById('js-closeButton');
-closeButton.addEventListener('click', () => {
-  mask.click();
-});
-function readUpToTheLastSentence(entries) {
-  if (!entries[0].isIntersecting) {
-    return;
-  }
-  checkboxToBeChecked();
-  addToggleToTheSubmitCheckbox();
-  mask.addEventListener('click', () => {
-    checkItemAndToggleSubmitButton(validation.isInvalidForLength, username, [
-      getStateOfItem(username),
-      minCharCount,
-      maxCharCount
-    ]);
-    checkItemAndToggleSubmitButton(validation.isInvalidForMail, email, [
-      getStateOfItem(email)
-    ]);
-    checkItemAndToggleSubmitButton(validation.isNotMatchPasswords, password, [
-      confirmPassword,
-      getStateOfItem(confirmPassword)
-    ]);
-    checkPasswordAndToggleSubmitButton();
-  });
-}
-submitButton.addEventListener('click', e => {
-  e.preventDefault();
-  window.location.href = 'registration.html';
-});
-const lastSentence = document.getElementById('js-lastSentence');
-const options = {
-  root: modal,
-  threshold: 1
-};
-const observer = new IntersectionObserver(readUpToTheLastSentence, options);
-observer.observe(lastSentence);
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
-
 let stateOfItems = [
   { item: username, empty: true, result: false },
   { item: email, empty: true, result: false },
   { item: password, empty: true, result: false },
   { item: confirmPassword, empty: true, result: false }
 ];
-
 function getStateOfItem(input) {
   return stateOfItems.find(obj => obj.item === input);
 }
+
+const submitButton = document.getElementById('js-submitButton');
+
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+  window.location.href = 'registration.html';
+});
 
 function checkItemAndToggleSubmitButton(func, arg, anotherArgs) {
   submitButton.disabled = true;
@@ -98,10 +31,8 @@ function checkItemAndToggleSubmitButton(func, arg, anotherArgs) {
   if (!submitCheckbox.checked) return;
   submitButton.disabled = false;
 }
-
 const minCharCount = 3;
 const maxCharCount = 15;
-
 username.addEventListener('input', () => {
   checkItemAndToggleSubmitButton(validation.isInvalidForLength, username, [
     getStateOfItem(username),
@@ -109,20 +40,17 @@ username.addEventListener('input', () => {
     maxCharCount
   ]);
 });
-
 email.addEventListener('input', () => {
   checkItemAndToggleSubmitButton(validation.isInvalidForMail, email, [
     getStateOfItem(email)
   ]);
 });
-
 function checkMatchingPasswordsAndToggleSubmitButton() {
   checkItemAndToggleSubmitButton(validation.isNotMatchPasswords, password, [
     confirmPassword,
     getStateOfItem(confirmPassword)
   ]);
 }
-
 function checkPasswordAndToggleSubmitButton() {
   checkItemAndToggleSubmitButton(validation.isInvalidForPassword, password, [
     getStateOfItem(password)
@@ -139,3 +67,89 @@ function checkPasswordAndToggleSubmitButton() {
 password.addEventListener('input', () => {
   checkPasswordAndToggleSubmitButton();
 });
+
+const linkToRule = document.getElementById('js-linkToRule');
+const mask = document.getElementById('js-mask');
+const modal = document.getElementById('js-modal');
+const modalBody = document.getElementById('js-modalBody');
+
+linkToRule.addEventListener('click', event => {
+  event.preventDefault();
+  mask.classList.remove('hidden');
+  modal.classList.remove('hidden');
+  modalBody.focus();
+});
+
+function closeModal() {
+  mask.classList.add('hidden');
+  modal.classList.add('hidden');
+}
+
+mask.addEventListener('click', () => {
+  closeModal();
+  username.focus();
+});
+
+const closeButton = document.getElementById('js-closeButton');
+
+closeButton.addEventListener('click', () => {
+  mask.click();
+});
+
+const submitCheckbox = document.getElementById('js-submitCheckbox');
+
+function checkboxToBeChecked() {
+  submitCheckbox.disabled = false;
+  submitCheckbox.checked = true;
+  if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
+  submitButton.disabled = false;
+}
+
+function addToggleToTheSubmitCheckbox() {
+  submitCheckbox.addEventListener('change', () => {
+    if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
+    if (!submitCheckbox.checked) {
+      submitButton.disabled = true;
+    } else {
+      submitButton.disabled = false;
+    }
+  });
+}
+
+function checkAllItems() {
+  checkItemAndToggleSubmitButton(validation.isInvalidForLength, username, [
+    getStateOfItem(username),
+    minCharCount,
+    maxCharCount
+  ]);
+  checkItemAndToggleSubmitButton(validation.isInvalidForMail, email, [
+    getStateOfItem(email)
+  ]);
+  checkItemAndToggleSubmitButton(validation.isNotMatchPasswords, password, [
+    confirmPassword,
+    getStateOfItem(confirmPassword)
+  ]);
+  checkPasswordAndToggleSubmitButton();
+}
+
+function readUpToTheLastSentence(entries) {
+  if (!entries[0].isIntersecting) {
+    return;
+  }
+  checkboxToBeChecked();
+  addToggleToTheSubmitCheckbox();
+  mask.addEventListener('click', () => {
+    checkAllItems();
+  });
+}
+
+const options = {
+  root: modal,
+  threshold: 1
+};
+
+const observer = new IntersectionObserver(readUpToTheLastSentence, options);
+
+const lastSentence = document.getElementById('js-lastSentence');
+
+observer.observe(lastSentence);
