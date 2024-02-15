8e4768bd3c3ad457349f131e5124cd869cefdf6f
groove <vacation555@gmail.com>
2024-02-15 22:10:01 +0900

    feat:revert to 56ad166

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  singleQuote: true,
-  trailingComma: "none",
-  arrowParens: "avoid",
-  proseWrap: "preserve",
-  quoteProps: "as-needed",
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: none,
+  trailingComma: 'true',
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
   bracketSameLine: false,
   bracketSpacing: true,
-  tabWidth: 2,
+  tabWidth: 2
 };
