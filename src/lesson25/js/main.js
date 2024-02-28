9f5d3826497831f4a9c776888ac0b913e55952dd
groove <vacation555@gmail.com>
2024-02-29 07:37:03 +0900

    feat:fix wrong changes

--- src/lesson25/js/main.js
+++ src/lesson25/js/main.js
@@ -1,7 +1,84 @@
 import * as validation from './modules/validation.js';
 
 const username = document.getElementById('username');
+
 username.focus();
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+
+let stateOfItems = [
+  { item: username, empty: true, isValid: false },
+  { item: email, empty: true, isValid: false },
+  { item: password, empty: true, isValid: false },
+  { item: confirmPassword, empty: true, isValid: false }
+];
+
+function getStateOfItem(input) {
+  return stateOfItems.find(obj => obj.item === input);
+}
+
+const submitButton = document.getElementById('js-submitButton');
+
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+  window.location.href = 'registration.html';
+});
+
+function checkItemAndToggleSubmitButton(func, arg, theOtherArgs) {
+  submitButton.disabled = true;
+  if (validation.showEmptyError(arg, getStateOfItem(arg))) return;
+  if (func(arg, ...theOtherArgs)) return;
+  if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
+  if (!submitCheckbox.checked) return;
+  submitButton.disabled = false;
+}
+
+const minCharCount = 3;
+const maxCharCount = 15;
+
+username.addEventListener('input', () => {
+  checkItemAndToggleSubmitButton(
+    validation.showResultUsernameValidation,
+    username,
+    [getStateOfItem(username), minCharCount, maxCharCount]
+  );
+});
+
+email.addEventListener('input', () => {
+  checkItemAndToggleSubmitButton(validation.showResultEmailValidation, email, [
+    getStateOfItem(email)
+  ]);
+});
+
+function checkMatchingPasswordsAndToggleSubmitButton() {
+  checkItemAndToggleSubmitButton(
+    validation.showResultMatchingPasswords,
+    password,
+    [confirmPassword, getStateOfItem(confirmPassword)]
+  );
+}
+
+function checkPasswordAndToggleSubmitButton() {
+  checkItemAndToggleSubmitButton(
+    validation.showResultPasswordValidation,
+    password,
+    [getStateOfItem(password)]
+  );
+  if (!getStateOfItem(password).isValid) return;
+  if (confirmPassword.value) checkMatchingPasswordsAndToggleSubmitButton();
+  confirmPassword.addEventListener('input', () => {
+    checkMatchingPasswordsAndToggleSubmitButton();
+  });
+  password.addEventListener('input', () => {
+    checkMatchingPasswordsAndToggleSubmitButton();
+  });
+}
+
+password.addEventListener('input', () => {
+  checkPasswordAndToggleSubmitButton();
+});
 
 const linkToRule = document.getElementById('js-linkToRule');
 const mask = document.getElementById('js-mask');
@@ -15,22 +92,34 @@ linkToRule.addEventListener('click', event => {
   modalBody.focus();
 });
 
-const submitCheckbox = document.getElementById('js-submitCheckbox');
-const submitButton = document.getElementById('js-submitButton');
-
-function checkboxToBeChecked() {
-  submitCheckbox.disabled = false;
-  submitCheckbox.checked = true;
-  if (!validation.isEveryRequiredItemValid()) return;
-  submitButton.disabled = false;
-}
 function closeModal() {
   mask.classList.add('hidden');
   modal.classList.add('hidden');
 }
+
+mask.addEventListener('click', () => {
+  closeModal();
+  username.focus();
+});
+
+const closeButton = document.getElementById('js-closeButton');
+
+closeButton.addEventListener('click', () => {
+  mask.click();
+});
+
+const submitCheckbox = document.getElementById('js-submitCheckbox');
+
+function checkboxToBeChecked() {
+  submitCheckbox.disabled = false;
+  submitCheckbox.checked = true;
+  if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
+  submitButton.disabled = false;
+}
+
 function addToggleToTheSubmitCheckbox() {
   submitCheckbox.addEventListener('change', () => {
-    if (!validation.isEveryRequiredItemValid()) return;
+    if (!validation.isEveryRequiredItemValid(stateOfItems)) return;
     if (!submitCheckbox.checked) {
       submitButton.disabled = true;
     } else {
@@ -38,72 +127,42 @@ function addToggleToTheSubmitCheckbox() {
     }
   });
 }
-mask.addEventListener('click', () => {
-  closeModal();
-  username.focus();
-});
 
-const closeButton = document.getElementById('js-closeButton');
-closeButton.addEventListener('click', () => {
-  mask.click();
-});
+function checkAllItems() {
+  checkItemAndToggleSubmitButton(
+    validation.showResultUsernameValidation,
+    username,
+    [getStateOfItem(username), minCharCount, maxCharCount]
+  );
+  checkItemAndToggleSubmitButton(validation.showResultEmailValidation, email, [
+    getStateOfItem(email)
+  ]);
+  checkItemAndToggleSubmitButton(
+    validation.showResultMatchingPasswords,
+    password,
+    [confirmPassword, getStateOfItem(confirmPassword)]
+  );
+  checkPasswordAndToggleSubmitButton();
+}
+
 function readUpToTheLastSentence(entries) {
   if (!entries[0].isIntersecting) {
     return;
   }
   checkboxToBeChecked();
   addToggleToTheSubmitCheckbox();
+  mask.addEventListener('click', () => {
+    checkAllItems();
+  });
 }
 
-submitButton.addEventListener('click', e => {
-  e.preventDefault();
-  window.location.href = 'registration.html';
-});
-const lastSentence = document.getElementById('js-lastSentence');
 const options = {
   root: modal,
   threshold: 1
 };
+
 const observer = new IntersectionObserver(readUpToTheLastSentence, options);
+
+const lastSentence = document.getElementById('js-lastSentence');
+
 observer.observe(lastSentence);
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const confirmPassword = document.getElementById('confirmPassword');
-function checkItemAndToggleSubmitButton(func, arg, anotherArgs) {
-  submitButton.disabled = true;
-  if (validation.isEmptyForRequired(arg)) return;
-  if (func(arg, ...anotherArgs)) return;
-  if (!validation.isEveryRequiredItemValid()) return;
-  if (!submitCheckbox.checked) return;
-  submitButton.disabled = false;
-}
-username.addEventListener('input', () => {
-  const minCharCount = 3;
-  const maxCharCount = 15;
-  checkItemAndToggleSubmitButton(validation.isInvalidForLength, username, [
-    minCharCount,
-    maxCharCount
-  ]);
-});
-email.addEventListener('input', () => {
-  checkItemAndToggleSubmitButton(validation.isInvalidForMail, email, []);
-});
-function checkMatchingPasswordsAndToggleSubmitButton() {
-  checkItemAndToggleSubmitButton(validation.isNotMatchPasswords, password, [
-    confirmPassword
-  ]);
-}
-function checkPasswordAndToggleSubmitButton() {
-  checkItemAndToggleSubmitButton(validation.isInvalidForPassword, password, []);
-  if (!validation.getStateOfItem(password).result) return;
-  if (confirmPassword.value) checkMatchingPasswordsAndToggleSubmitButton();
-  confirmPassword.addEventListener('input', () => {
-    checkMatchingPasswordsAndToggleSubmitButton();
-  });
-  password.addEventListener('input', () => {
-    checkMatchingPasswordsAndToggleSubmitButton();
-  });
-}
-password.addEventListener('input', () => {
-  checkPasswordAndToggleSubmitButton();
-});
--- src/lesson25/js/modules/validation.js
+++ src/lesson25/js/modules/validation.js
@@ -1,42 +1,32 @@
-export let stateOfItems = [
-  { item: username, empty: true, result: false },
-  { item: email, empty: true, result: false },
-  { item: password, empty: true, result: false },
-  { item: confirmPassword, empty: true, result: false }
-];
-
-export function getStateOfItem(input) {
-  return stateOfItems.find(obj => obj.item === input);
-}
-
-export function showError(input, message) {
+export function showError(input, stateOfItem, message) {
   input.classList.remove('border-gray-200');
   input.classList.remove('border-successColor');
   input.classList.add('border-errorColor');
-  const stateOfItem = getStateOfItem(input);
-  stateOfItem.result = false;
+  stateOfItem.isValid = false;
   const parentElement = input.parentElement;
   const small = parentElement.querySelector('small');
   small.innerText = message;
   small.classList.remove('invisible');
 }
 
-export function showSuccess(input) {
+export function showSuccess(input, stateOfItem) {
   input.classList.remove('border-gray-200');
   input.classList.remove('border-errorColor');
   input.classList.add('border-successColor');
-  const stateOfItem = getStateOfItem(input);
-  stateOfItem.result = true;
+  stateOfItem.isValid = true;
   const parentElement = input.parentElement;
   const small = parentElement.querySelector('small');
   small.classList.add('invisible');
 }
 
-export function isEmptyForRequired(input) {
-  const stateOfItem = getStateOfItem(input);
+export function showEmptyError(input, stateOfItem) {
   if (input.value.trim() === '') {
     stateOfItem.empty = true;
-    showError(input, `${capitalizeTheFirstLetter(input)} is required`);
+    showError(
+      input,
+      stateOfItem,
+      `${capitalizeTheFirstLetter(input)} is required`
+    );
     return true;
   } else {
     stateOfItem.empty = false;
@@ -53,64 +43,60 @@ export function removeErrorMessages(inputArr) {
   });
 }
 
-export function checkForUnfilled(input) {
-  const stateOfItem = getStateOfItem(input);
+export function isEmpty(stateOfItem) {
   if (stateOfItem.empty) return true;
 }
 
-export function isInvalidForLength(input, min, max) {
-  if (checkForUnfilled(input)) return;
+export function showResultUsernameValidation(input, stateOfItem, min, max) {
+  if (isEmpty(input)) return;
   if (input.value.length < min) {
     showError(
       input,
+      stateOfItem,
       `${capitalizeTheFirstLetter(input)} must be at least ${min} characters`
     );
-    return true;
   } else if (input.value.length > max) {
     showError(
       input,
+      stateOfItem,
       `${capitalizeTheFirstLetter(input)} must be less than ${max} characters`
     );
-    return true;
   } else {
-    showSuccess(input);
-    return false;
+    showSuccess(input, stateOfItem);
   }
 }
 
-export function isInvalidForMail(input) {
-  if (checkForUnfilled(input)) return;
+export function showResultEmailValidation(input, stateOfItem) {
+  if (isEmpty(input)) return;
   const regularExpression = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
   // https://stackoverflow.com/questions/65801147/validate-email-pattern-with-regex
   if (regularExpression.test(input.value.trim())) {
-    showSuccess(input);
-    return false;
+    showSuccess(input, stateOfItem);
   } else {
-    showError(input, 'Email is not valid');
-    return true;
+    showError(input, stateOfItem, 'Email is not valid');
   }
 }
 
-export function isInvalidForPassword(input) {
-  if (checkForUnfilled(input)) return;
+export function showResultPasswordValidation(input, stateOfItem) {
+  if (isEmpty(input)) return;
   const regularExpression = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
   // https://genkichi.hateblo.jp/entry/2019/02/23/143527
   if (regularExpression.test(input.value.trim())) {
-    showSuccess(input);
-    return false;
+    showSuccess(input, stateOfItem);
   } else {
-    showError(input, 'Min 8 chars, including 1 uppercase and 1 number');
-    return true;
+    showError(
+      input,
+      stateOfItem,
+      'Min 8 chars, including 1 uppercase and 1 number'
+    );
   }
 }
 
-export function isNotMatchPasswords(input, confirmInput) {
+export function showResultMatchingPasswords(input, confirmInput, stateOfItem) {
   if (input.value === confirmInput.value) {
-    showSuccess(confirmInput);
-    return false;
+    showSuccess(confirmInput, stateOfItem);
   } else {
-    showError(confirmInput, 'Passwords do not match');
-    return true;
+    showError(confirmInput, stateOfItem, 'Passwords do not match');
   }
 }
 
@@ -118,6 +104,6 @@ export function capitalizeTheFirstLetter(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-export function isEveryRequiredItemValid() {
-  return stateOfItems.every(obj => obj.result === true);
+export function isEveryRequiredItemValid(stateOfItems) {
+  return stateOfItems.every(obj => obj.isValid);
 }
