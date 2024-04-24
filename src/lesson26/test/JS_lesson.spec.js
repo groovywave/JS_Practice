d4ad134d76e08a5fd7f006d8160e019d380c5572
groove <vacation555@gmail.com>
2024-04-24 12:34:27 +0900

    feat:add test code

--- src/lesson26/test/JS_lesson.spec.js
+++ src/lesson26/test/JS_lesson.spec.js
@@ -6,7 +6,8 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('tab-key navigation', () => {
   test.beforeEach(async ({ page }) => {
-    await page.getByLabel('Username').click();
+    await page.getByRole('button', { name: 'Registration' }).click();
+    await page.getByText('Username').click();
     await page.getByLabel('Username').press('Tab');
     await page.getByLabel('Email').press('Tab');
     await page.getByTestId('js-passwordLabel').press('Tab');
@@ -27,6 +28,7 @@ test.describe('form validation', () => {
   test('input less than 3 characters into username and show error message', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByLabel('Username').fill('e');
     await expect(
       page.getByTestId('js-usernameError', {
@@ -38,6 +40,7 @@ test.describe('form validation', () => {
   test('input more than 15 characters into username and show error message', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByLabel('Username').fill('01234567890123456789');
     await expect(
       page.getByTestId('js-usernameError', {
@@ -49,6 +52,7 @@ test.describe('form validation', () => {
   test('input invalid mail address into email and show error message', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByLabel('Email').fill('invalid email');
     await expect(
       page.getByTestId('js-emailError', {
@@ -60,6 +64,7 @@ test.describe('form validation', () => {
   test('input less than 8 characters into Password and show error message', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByTestId('js-passwordLabel').fill('abc');
     await expect(
       page.getByTestId('js-passwordError', {
@@ -71,6 +76,7 @@ test.describe('form validation', () => {
   test('input wrong password into Confirm Password and show error message', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByTestId('js-passwordLabel').fill('KevinDeBruyne17');
     await page.getByTestId('js-confirmPasswordLabel').fill('KevinDeBruyne7');
     await expect(
@@ -83,6 +89,7 @@ test.describe('form validation', () => {
   test('Show a completion message after the form is successfully submitted', async ({
     page
   }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
     await page.getByRole('link', { name: '利用規約' }).press('Enter');
     await page.getByTestId('js-lastSentence').scrollIntoViewIfNeeded();
     await page.getByTestId('js-lastSentence').press('ArrowDown');
@@ -95,6 +102,122 @@ test.describe('form validation', () => {
     await expect(
       page.getByRole('heading', {
         name: 'Your Registration Has Been Completed!'
+      })
+    ).toBeVisible();
+  });
+});
+
+test.describe('input for registration', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Registration' }).click();
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').press('Tab');
+    await page.getByLabel('Email').press('Tab');
+    await page.getByTestId('js-passwordLabel').press('Tab');
+    await page.getByTestId('js-confirmPasswordLabel').press('Tab');
+  });
+
+  test('press tab-key and focus on 利用規約', async ({ page }) => {
+    await expect(page.getByRole('link', { name: '利用規約' })).toBeFocused();
+  });
+
+  test('press Enter on the link and open the modal', async ({ page }) => {
+    await page.getByRole('link', { name: '利用規約' }).press('Enter');
+    await expect(page.getByTestId('js-modalBody')).toBeVisible();
+  });
+});
+
+test.describe('input for login', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Login' }).click();
+  });
+  test('show login form', async ({ page }) => {
+    await expect(page.getByText('Username or Email')).toBeVisible();
+  });
+  test('input required items and click login button', async ({ page }) => {
+    await page.getByTestId('test-usernameOrEmail').fill('Hoeger');
+    await page.getByTestId('test-password').fill('HQnmjPKBWkqzjeB');
+    await page.getByTestId('test-loginButton').click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Contents'
+      })
+    ).toBeVisible();
+  });
+  test('do not input required items and click login button', async ({
+    page
+  }) => {
+    await page.getByTestId('test-loginButton').click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Login failed'
+      })
+    ).toBeVisible();
+  });
+  test('click link forgot password?', async ({ page }) => {
+    await page.getByRole('link', { name: 'Forgot password?' }).click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Forgot password ?'
+      })
+    ).toBeVisible();
+  });
+});
+
+test.describe('behavior of local storage', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('button', { name: 'Login' }).click();
+  });
+  test('input required items and click login button', async ({ page }) => {
+    await page.getByTestId('test-usernameOrEmail').fill('Hoeger');
+    await page.getByTestId('test-password').fill('HQnmjPKBWkqzjeB');
+    await page.getByTestId('test-loginButton').click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Contents'
+      })
+    ).toBeVisible();
+  });
+  test('login with local storage', async ({ page }) => {
+    await page.getByTestId('test-usernameOrEmail').fill('Hoeger');
+    await page.getByTestId('test-password').fill('HQnmjPKBWkqzjeB');
+    await page.getByTestId('test-loginButton').click();
+    await page.goBack;
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Contents'
+      })
+    ).toBeVisible();
+  });
+
+  test('do not input required items and click login button', async ({
+    page
+  }) => {
+    await page.getByTestId('test-loginButton').click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Login failed'
+      })
+    ).toBeVisible();
+  });
+  test('click the link named forgot password?', async ({ page }) => {
+    await page.getByRole('link', { name: 'Forgot password?' }).click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Forgot password ?'
+      })
+    ).toBeVisible();
+  });
+  test('logout and login again', async ({ page }) => {
+    await page.getByTestId('test-usernameOrEmail').fill('Hoeger');
+    await page.getByTestId('test-password').fill('HQnmjPKBWkqzjeB');
+    await page.getByTestId('test-loginButton').click();
+    await page.getByTestId('test-logout').click();
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(
+      page.getByRole('heading', {
+        name: 'Login'
       })
     ).toBeVisible();
   });
