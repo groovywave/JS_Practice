877c0e53a56104ca05556061944c92a71f6a8850
groove <vacation555@gmail.com>
2024-04-27 15:44:28 +0900

    feat:add login.js, registration.js and some html files

---
+++ src/lesson26/js/index.js
@@ -0,0 +1,13 @@
+const loginButton = document.getElementById('js-loginButton');
+const registrationButton = document.getElementById('js-registrationButton');
+console.log(loginButton);
+console.log(registrationButton);
+loginButton.addEventListener('click', e => {
+  e.preventDefault();
+  window.location.href = 'login.html';
+});
+
+registrationButton.addEventListener('click', e => {
+  e.preventDefault();
+  window.location.href = 'registration.html';
+});
