d9adea116c2bb3e8a0730d6d97b916e6db760dc1
groove <vacation555@gmail.com>
2024-05-29 19:10:48 +0900

    feat: remove unnecessary code

--- src/lesson26/js/modules/renderCircle.js
+++ src/lesson26/js/modules/renderCircle.js
@@ -1,16 +1,7 @@
 export function renderCircle(element) {
-  // const loadingCircleContainer = document.createElement("div");
-  // loadingCircleContainer.id = "js-loadingCircleContainer";
-  // loadingCircleContainer.className = "loading-circle-container";
-  // const loadingCircle = document.createElement("img");
-  // loadingCircle.src = "/assets/img/loading-circle.gif";
-  // loadingCircle.alt = "ローディング画像";
-  // element.appendChild(loadingCircleContainer).appendChild(loadingCircle);
   const loadingCircleContainer = document.createElement('div');
   loadingCircleContainer.id = 'js-loadingCircleContainer';
   loadingCircleContainer.classList.add('loading-circle-container');
-  // loadingCircleContainer.classList.add('flex');
-  // loadingCircleContainer.classList.add('justify-center');
   const loadingCircle = document.createElement('img');
   loadingCircle.classList.add('loading-circle');
   loadingCircle.src = '/assets/img/loading-circle.gif';
