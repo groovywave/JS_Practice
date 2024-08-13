967ed6b64375a559ee8e342367cd6d2a92bf2551
groove <vacation555@gmail.com>
2024-08-13 17:00:05 +0900

    feat:fetch all user data compare it with the submitted data

--- src/lesson28/js/login.js
+++ src/lesson28/js/login.js
@@ -5,20 +5,11 @@ const usernameOrEmail = document.getElementById('js-usernameOrEmail');
 const password = document.getElementById('js-password');
 const loginButton = document.getElementById('js-loginButton');
 
-if (localStorage.getItem('token') === 'ae2efaa8fd0255cfafda76a7')
-  window.location.href = './contents.html';
+if (localStorage.getItem('token')) window.location.href = './contents.html';
 
 loginButton.addEventListener('click', async e => {
   e.preventDefault();
-  const queryStringOfUsernameOrEmail = encodeURIComponent(
-    usernameOrEmail.value
-  );
-  const queryStringOfPassword = encodeURIComponent(password.value);
-  // https://stackoverflow.com/questions/65801147/validate-email-pattern-with-regex
-  let response;
   try {
-    const regularExpression =
-      /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     validation.removeErrorMessages([usernameOrEmail, password]);
     validation.removeErrorColor([usernameOrEmail, password]);
     if (usernameOrEmail.value === '') {
@@ -29,20 +20,36 @@ loginButton.addEventListener('click', async e => {
       validation.showError(password, 'No data');
       return;
     }
-    if (!regularExpression.test(usernameOrEmail.value.trim())) {
-      response = await fetch(
-        `${url}?name=${queryStringOfUsernameOrEmail}&password=${queryStringOfPassword}`
-      );
-    } else {
-      response = await fetch(
-        `${url}?email=${queryStringOfUsernameOrEmail}&password=${queryStringOfPassword}`
-      );
-    }
+    const response = await fetch(`${url}`);
     if (!response.ok) {
       window.location.href = './login-failed.html';
       return;
     }
-    localStorage.setItem('token', 'ae2efaa8fd0255cfafda76a7');
+    const responseData = await response.json();
+    const regularExpression =
+      /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    // https://stackoverflow.com/questions/65801147/validate-email-pattern-with-regex
+    let isName = true;
+    if (regularExpression.test(usernameOrEmail.value.trim())) isName = false;
+    const matchedUserData = await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(
+          responseData.find(obj => {
+            return (
+              obj.password === password.value &&
+              (isName
+                ? obj.name === usernameOrEmail.value
+                : obj.email === usernameOrEmail.value)
+            );
+          })
+        );
+      }, 1000);
+    });
+    if (!matchedUserData) {
+      window.location.href = './login-failed.html';
+      return;
+    }
+    localStorage.setItem('token', matchedUserData.userId);
     window.location.href = './contents.html';
   } catch (error) {
     console.error(error);
