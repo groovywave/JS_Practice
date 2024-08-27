ff74618714a4e3cc4c9026eeadaa22f1f403a8c4
groove <vacation555@gmail.com>
2024-08-27 23:16:37 +0900

    test: Add a test Submit button toBeDisabled

--- src/lesson28/test/reset-password.spec.js
+++ src/lesson28/test/reset-password.spec.js
@@ -12,6 +12,7 @@ test.beforeEach(async ({ page }) => {
   await page.getByTestId('test-buttonForResetPassword').click();
   await page.getByPlaceholder('Enter password', { exact: true }).click();
 });
+
 test('Input password and confirmPassword correctly and then to be visible the submit button', async ({
   page,
 }) => {
@@ -20,55 +21,61 @@ test('Input password and confirmPassword correctly and then to be visible the su
     .fill('111111Aa');
   await page.getByPlaceholder('Enter password again').click();
   await page.getByPlaceholder('Enter password again').fill('111111Aa');
-  await expect(page.getByRole('button', { name: 'Submit' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Submit' })).toBeEnabled();
 });
-test('Input password less than 8 letters and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('12345Aa');
-  await expect(page.getByTestId('test-passwordError')).toBeVisible();
-});
-test('Input password without without uppercase and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('123456aa');
-  await expect(page.getByTestId('test-passwordError')).toBeVisible();
-});
-test('Input password without lowercase and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('123456AA');
-  await expect(page.getByTestId('test-passwordError')).toBeVisible();
-});
-test('Input password without alphabets and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('12345678');
-  await expect(page.getByTestId('test-passwordError')).toBeVisible();
-});
-test('Input password without numbers and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('abcdefgH');
-  await expect(page.getByTestId('test-passwordError')).toBeVisible();
-});
-test('ConfirmPassword does not match with Password and show an error message', async ({
-  page,
-}) => {
-  await page
-    .getByPlaceholder('Enter password', { exact: true })
-    .fill('111111Aa');
-  await page.getByPlaceholder('Enter password again').click();
-  await page.getByPlaceholder('Enter password again').fill('111111A');
-  await expect(page.getByTestId('test-confirmPasswordError')).toBeVisible();
+
+test.describe('Show validation Error', () => {
+  test.afterEach(async ({ page }) => {
+    await expect(page.getByRole('button', { name: 'Submit' })).toBeDisabled();
+  });
+  test('Input password less than 8 letters and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('12345Aa');
+    await expect(page.getByTestId('test-passwordError')).toBeVisible();
+  });
+  test('Input password without uppercase and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('123456aa');
+    await expect(page.getByTestId('test-passwordError')).toBeVisible();
+  });
+  test('Input password without lowercase and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('123456AA');
+    await expect(page.getByTestId('test-passwordError')).toBeVisible();
+  });
+  test('Input password without alphabets and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('12345678');
+    await expect(page.getByTestId('test-passwordError')).toBeVisible();
+  });
+  test('Input password without numbers and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('abcdefgH');
+    await expect(page.getByTestId('test-passwordError')).toBeVisible();
+  });
+  test('ConfirmPassword does not match with Password and show an error message', async ({
+    page,
+  }) => {
+    await page
+      .getByPlaceholder('Enter password', { exact: true })
+      .fill('111111Aa');
+    await page.getByPlaceholder('Enter password again').click();
+    await page.getByPlaceholder('Enter password again').fill('111111A');
+    await expect(page.getByTestId('test-confirmPasswordError')).toBeVisible();
+  });
 });
